b70f31e1b3cffa359f7d2fb7cb8f42098549691e
Moonart19 <mm4u005@gmail.com>
2025-04-30 18:10:21 +0530

    Added random color and darken feature

--- script.js
+++ script.js
@@ -13,32 +13,80 @@ function createGrid(gridNum) {
         div.style.width = `${cellDimensions}px`;
         sketchArea.appendChild(div);
     }
+    
     const cells = document.querySelectorAll(".cell");
-
     cells.forEach(cell => {
-        cell.addEventListener('mouseenter', () => {
-            cell.style.backgroundColor = "black";
+        cell.addEventListener('mouseover', () => {
+            cell.style.backgroundColor = "orange";
+            cell.style.border = "1px ridge blue";
         });
     });
 }
 
-function clear(gridNum) {
-    const children= sketchArea.querySelectorAll(".cell");
-    children.forEach(child => {
-        sketchArea.removeChild(child);
+    function applyColor(colorFunction) {
+        darkOn = false;
+        const cells = document.querySelectorAll(".cell");
+        cells.forEach(cell => {
+            cell.addEventListener('mouseover', () => {
+                cell.style.backgroundColor = colorFunction();
+                cell.style.opacity = 1;
+            });
+        });
+    }
+
+
+    const orangeButton = document.querySelector(".orange-color");
+    orangeButton.addEventListener('click', () => applyColor(() => "orange"));
+
+    const randomButton = document.querySelector(".random-color");
+    randomButton.addEventListener('click', () => applyColor(randomColor));
+    
+    const darkenButton = document.querySelector(".darken-color");
+    let darkOn = true;
+    darkenButton.addEventListener('click', () => { 
+        darkOn = true;
+        const cells = document.querySelectorAll(".cell");
+        cells.forEach(cell => {
+            let opacityLevel = 0;
+            cell.addEventListener('mouseover', () => {
+                if(darkOn) {
+                    if(opacityLevel < 1) {
+                        opacityLevel += 0.1;
+                        cell.style.opacity = opacityLevel;
+                    }
+                }
+        });
     });
 
-    createGrid(gridNum)
-}
-
-const editGrid = document.querySelector(".edit-grid");
-editGrid.addEventListener("click", () => {
-    const gridNum = +prompt("Enter the number of squares per side");
-    if(gridNum > 0 && gridNum < 100)
-        clear(gridNum);
-    else
-        alert("Can't Do it blah!");
 });
+
+    
+
+    function clear(gridNum) {
+        const children= sketchArea.querySelectorAll(".cell");
+        children.forEach(child => {
+            sketchArea.removeChild(child);
+        });
+        
+        createGrid(gridNum);
+    }
+    
+    const editGrid = document.querySelector(".edit-grid");
+    editGrid.addEventListener("click", () => {
+        const gridNum = +prompt("Enter the number of squares per side");
+        if(gridNum > 0 && gridNum < 100)
+            clear(gridNum);
+        else
+            alert("Please enter a number between 1 and 100!");
+});
+
+
+    function randomColor () {
+        let random = () => {
+            return (Math.random() * 255).toFixed(0);
+        }
+        return `rgb(${random()},${random()},${random()})`;
+    }
 
 
 createGrid(16);
